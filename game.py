286efa168f35061d7e1f7e9836d17b9150d0aec4
youssef <youssefyoussef.m4@gmail.com>
2024-08-19 17:54:29 +0300

    add restart game functionality

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ class Player:
         self.num=random.randint(1,3)
     def win_round(self):
         self.score+=1
+    def reset(self):
+        self.score=0
 
     def get_num(self):
         return self.num
@@ -26,6 +28,20 @@ class Game:
         print("Game has begun!")
     def end(self):
         print("Game has ended!")
+    def restart(self):
+        while True:
+            user_input = int(input("do you want to play again? (1 yes,0 no)"))
+            if user_input in [1,0]:
+                break
+            else:
+                print("Invalid input. Please enter 1 or 0.")
+        if user_input:
+            self.trials=0
+            self.p1.reset()
+            self.p2.reset()
+            self.loop()
+        else:
+            pass
     
     def display_round_result(self):
         print(f"Player 1 score: {self.p1.get_score()}\nPlayer 2 score: {self.p2.get_score()}\n___________________________")
@@ -45,9 +61,10 @@ class Game:
             self.p2.generate_num()
             self.play()
             self.display_round_result()
-            time.sleep(2)
+            time.sleep(1)
             self.trials+=1
         self.display_winner()
+        self.restart()
 
     def play(self):
         if(self.p1.get_num() == self.p2.get_num() ):
